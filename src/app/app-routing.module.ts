1139351b7380efb14c963863174ef32932f48e0a
Tobias Schramek <ts@wunschloesung.com>
2019-01-05 11:39:04 +0100

    [initial-setup] ADded routes to app-routing-module for lazyLoading routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,41 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './components/sites/home/home.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {
-    path: 'teaSorts',
-    loadChildren: './components/sites/tea-sorts/tea-sorts.module#TeaSortsModule'
-  }
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'teaSorts',
+        loadChildren: './components/sites/tea-sorts/tea-sorts.module#TeaSortsModule'
+    },
+    {
+        path: 'teaGrowingRegions',
+        loadChildren: './components/sites/tea-growing-regions/tea-growing-regions.module#TeaGrowingRegionsModule'
+    },
+    {
+        path: 'teaAromas',
+        loadChildren: './components/sites/tea-aromas/tea-aromas.module#TeaAromasModule'
+    },
+    {
+        path: 'teaProperties',
+        loadChildren: './components/sites/tea-properties/tea-properties.module#TeaPropertiesModule'
+    },
+    {
+        path: 'teaLeaveStructures',
+        loadChildren: './components/sites/tea-leave-structures/tea-leave-structures.module#TeaLeaveStructuresModule'
+    },
+    {
+        path: 'teaPlucking',
+        loadChildren: './components/sites/tea-plucking/tea-plucking.module#TeaPluckingModule'
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
