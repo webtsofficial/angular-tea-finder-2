7dc7a9ee00e3628f55532986949f6729213cbb68
Tobias Schramek <ts@wunschloesung.com>
2019-01-06 13:26:46 +0100

    [introduction-speech] added typed.js and basic usage in introduction-speech

--- src/app/components/finder/speeches/introduction-speech/introduction-speech.component.ts
+++ src/app/components/finder/speeches/introduction-speech/introduction-speech.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-introduction-speech',
@@ -10,6 +11,13 @@ export class IntroductionSpeechComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const typed = new Typed('.typed', {
+      stringsElement: '#typed-string',
+      smartBackspace: true,
+      typeSpeed: 30,
+      backDelay: 1000
+    });
+    console.log(typed);
   }
 
 }
