bf8517a6c649acdfeadc1749178f4aa0b2d3a4a8
Tobias Schramek <ts@wunschloesung.com>
2019-01-11 12:32:43 +0100

    [tea-growing-region] Aded Google Map Types (@types) and basic markup to place map in

--- src/app/components/tea/growing-region/growing-regions-maps-select/growing-regions-maps-select.component.ts
+++ src/app/components/tea/growing-region/growing-regions-maps-select/growing-regions-maps-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-growing-regions-maps-select',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./growing-regions-maps-select.component.scss']
 })
 export class GrowingRegionsMapsSelectComponent implements OnInit {
+  @ViewChild('gmap') gmapElment: any;
 
   constructor() { }
 
